dcb2acebb204c4be69013aaeda6d8aa16d4dcf95
Austin Brock <austinjbrock@gmail.com>
2022-09-15 21:28:47 -0700

    removed display component function

--- src/index.js
+++ src/index.js
@@ -2,16 +2,4 @@
 import createLayout from "./navbar";
 import './style.css';
 
-
-
-// function component() {
-//    const element = document.createElement('div');
-//    element.textContent = myName('Austin')
-//    element.classList.add('hello')
-   
-//    return element
-//  }
-//    document.body.appendChild(component());
-
-
 createLayout()
